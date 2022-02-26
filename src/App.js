c575e2eed04358be9653b77f67522acc95f24da3
Devanshi Sharma <devanshi.ds.20@gmail.com>
2022-02-26 11:01:50 +0530

    material ui login sign up

--- src/App.js
+++ src/App.js
@@ -2,11 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 import AuthProvider from './Context/AuthProvider';
 import SignUp from './Components/SignUp';
+import Main from './MaterialUI/Main';
 function App() {
   return (
-    <AuthProvider>
-      <SignUp/>
-    </AuthProvider>
+    <Main/>
   );
 }
 
---
+++ src/MaterialUI/Main.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavBar from "./NavBar"
+import Cards from "./Cards"
+import SignUp from "./SignUp"
+import Login from "./Login"
+function Main() {
+  return (
+    <div>
+        <SignUp/>
+    </div>
+  )
+}
+
+export default Main
